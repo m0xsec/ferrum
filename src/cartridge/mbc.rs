ce95f407545cffdec5a76b5e3c4de7f2b06783ba
m0x <m0x@m0x.io>
2023-04-22 22:32:50 -0700

    update MBC1 info from updated pandocs source

--- src/cartridge/mbc.rs
+++ src/cartridge/mbc.rs
@@ -41,32 +41,47 @@ enum BankMode {
 }
 
 /// https://gbdev.io/pandocs/MBC1.html
-/// This is the first MBC chip for the gameboy. Any newer MBC chips are working similiar, so that is relative easy to
-/// upgrade a program from one MBC chip to another - or even to make it compatible to several different types of MBCs.
-/// Note that the memory in range 0000-7FFF is used for both reading from ROM, and for writing to the MBCs Control
-/// Registers.
+/// This is the first MBC chip for the Game Boy. Any newer MBC chips work similarly, so it is relatively easy to upgrade a program from one MBC chip to
+/// another — or to make it compatible with several types of MBCs.
+///
+/// In its default configuration, MBC1 supports up to 512 KiB ROM with up to 32 KiB of banked RAM. Some cartridges wire the MBC differently, where the
+/// 2-bit RAM banking register is wired as an extension of the ROM banking register (instead of to RAM) in order to support up to 2 MiB ROM, at the cost
+/// of only supporting a fixed 8 KiB of cartridge RAM. All MBC1 cartridges with 1 MiB of ROM or more use this alternate wiring. Also see the note on
+/// MBC1M multi-game compilation carts below.
+///
+/// Note that the memory in range 0000–7FFF is used both for reading from ROM and writing to the MBCs Control Registers.
 ///
 /// 0000-3FFF - ROM Bank 00 (Read Only)
-/// This area always contains the first 16KBytes of the cartridge ROM.
+/// This area normally contains the first 16 KiB (bank 00) of the cartridge ROM.
 ///
 /// 4000-7FFF - ROM Bank 01-7F (Read Only)
-/// This area may contain any of the further 16KByte banks of the ROM, allowing to address up to 125 ROM Banks
-/// (almost 2MByte). As described below, bank numbers 20h, 40h, and 60h cannot be used, resulting in the odd amount of
-/// 125 banks.
+/// This area may contain any of the further 16 KiB banks of the ROM.
+/// If the main 5-bit ROM banking register is 0, it reads the bank as if it was set to 1.
+///
+/// For 1 MiB+ ROM, this means any bank that is possible to accessible via the 0000–3FFF region is not accessible in this region. i.e. banks $00/$20/$40/
+/// $60 in regular large ROM carts, or banks $00/$10/$20/$30 in MBC1M multi-game compilation carts. Instead, it automatically maps to 1 bank higher ($01/
+/// $21/$41/$61 or $01/$11/$21/$31 respectively).
 ///
 /// A000-BFFF - RAM Bank 00-03, if any (Read/Write)
-/// This area is used to address external RAM in the cartridge (if any). External RAM is often battery buffered,
-/// allowing to store game positions or high score tables, even if the gameboy is turned off, or if the cartridge is
-/// removed from the gameboy. Available RAM sizes are: 2KByte (at A000-A7FF), 8KByte (at A000-BFFF), and 32KByte (in
-/// form of four 8K banks at A000-BFFF).
+/// This area is used to address external RAM in the cartridge (if any).
+/// The RAM is only accessible if RAM is enabled, otherwise reads return open bus values (often $FF, but not guaranteed) and writes are ignored.
+///
+/// Available RAM sizes are 8 KiB (at $A000–BFFF) and 32 KiB (in form of four 8K banks at $A000–BFFF).
+/// 32 KiB is only available in cartridges with ROM <= 512 KiB.
+///
+/// External RAM is often battery-backed, allowing for the storage of game data while the Game Boy is turned off,
+/// or if the cartridge is removed from the Game Boy. External RAM is no slower than the Game Boy’s internal RAM,
+/// so many games use part of the external RAM as extra working RAM, even if they use another part of it for battery-backed saves.
+///
+/// Registers:
+/// All of the MBC1 registers default to $00 on power-up, which for the “ROM Bank Number” register is treated as $01.
 ///
 /// 0000-1FFF - RAM Enable (Write Only)
 /// Before external RAM can be read or written, it must be enabled by writing to this address space. It is recommended
 /// to disable external RAM after accessing it, in order to protect its contents from damage during power down of the
 /// gameboy. Usually the following values are used:
-///   00h  Disable RAM (default)
-///   0Ah  Enable RAM
-/// Practically any value with 0Ah in the lower 4 bits enables RAM, and any other value disables RAM.
+///   0x00  Disable RAM (default)
+///   0x0A  Enable RAM
 ///
 /// 2000-3FFF - ROM Bank Number (Write Only)
 /// Writing to this address space selects the lower 5 bits of the ROM Bank Number (in range 01-1Fh). When 00h is written,
